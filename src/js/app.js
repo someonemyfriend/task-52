6ad4e4eb3f7ed99578a469e3862f44878e2a86c4
Vasilia Staykova <someonemyfriend@gmail.com>
2023-01-14 15:39:28 +0200

    Add files via upload

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,22 @@
-import "../scss/app.scss";
+// import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+
+document.body.addEventListener("click", () => {
+  const length = 5;
+  let count = 0;
+  for(let i = 0; i < length; i++) {
+    let element = document.createElement('article');
+
+    count += length;
+    element.setAttribute('id', 'article' + count);
+    document.body.appendChild(element);
+    let text = 'sampleText';
+    document.getElementById('article' + count).innerHTML - text;
+    document.getElementById('article' + count).classList.add('message');
+  }
+})
 
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
